894eb0ca9e10909396190b70f88d3c201e642d75
tag101 <tag101@hanmail.net>
2025-03-15 15:01:00 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 13. 컴포넌트 생성 (Top 3 리스트 아이템)

---
+++ src/mocks/top-3-board-list.mock.ts
@@ -0,0 +1,42 @@
+import { BoardListItem } from "types/interface";
+
+const top3BoardListMock: BoardListItem[] = [
+    {
+        boardNumber: 1,
+        title: "강아지 산책길1",
+        content: "강아지 산책길 어디까지 갈까~~~~~~~~~~~~~~~~~~~~~~~~~~1",
+        boardTitleImage: "https://ssl.pstatic.net/melona/libs/1528/1528196/024497a3fde1da8f2fb3_20250313172250499.jpg",
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2025.03.13. 09:36:25",
+        writerNickname: "코린이",
+        writerProfileImage: "https://ssl.pstatic.net/melona/libs/1528/1528196/024497a3fde1da8f2fb3_20250313172250499.jpg",
+    },
+    {
+        boardNumber: 2,
+        title: "강아지 산책길2",
+        content: "강아지 산책길 어디까지 갈까~~~~~~~~~~~~~~~~~~~~~~~~~~2",
+        boardTitleImage: null,
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2025.03.13. 09:36:25",
+        writerNickname: "코린이",
+        writerProfileImage: null,
+    },
+    {
+        boardNumber: 3,
+        title: "강아지 산책길3",
+        content: "강아지 산책길 어디까지 갈까~~~~~~~~~~~~~~~~~~~~~~~~~~3",
+        boardTitleImage: null,
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2025.03.13. 09:36:25",
+        writerNickname: "코린이",
+        writerProfileImage: null,
+    }
+];
+
+export default top3BoardListMock;
