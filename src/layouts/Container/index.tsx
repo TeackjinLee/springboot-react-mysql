80e57183f5294c705419b770ea1bf80105b54b30
tag101 <tag101@hanmail.net>
2025-03-18 21:36:53 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 18. React-Router-Dom SPA 작업

---
+++ src/layouts/Container/index.tsx
@@ -0,0 +1,21 @@
+import Header from "../Header";
+import Footer from "../Footer";
+import {Outlet, useLocation} from "react-router-dom";
+import {AUTH_PATH} from "../../constant";
+
+//      component: layout      //
+export default function Container() {
+
+    //      state: 현재 페이지 path name 상태  //
+    const { pathname } = useLocation();
+
+    //      render: layout render      //
+    return (
+        <>
+            {pathname}
+            <Header />
+            <Outlet />
+            {pathname !== AUTH_PATH() && <Footer />}
+        </>
+    )
+}
