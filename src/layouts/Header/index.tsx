991fdd877752f80abc2a60020979b3ae8d15fbd2
tag101 <tag101@hanmail.net>
2025-03-19 23:48:00 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 19. Header 레이아웃 작성 A

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,7 +1,7 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, useRef, useState} from 'react';
 import './style.css';
 import {useNavigate} from "react-router-dom";
-import {MAIN_PATH} from "../../constant";
+import {MAIN_PATH, SEARCH_PATH} from "../../constant";
 
 //      component: Header layout      //
 export default function Header() {
@@ -16,43 +16,52 @@ export default function Header() {
 
     //      component: 검색 버튼 컴포넌트       //
     const SearchButton = () => {
+        //      state: 검색 버튼 요소 참조 상태     //
+        const searchButtonRef = useRef<HTMLDivElement | null>(null);
 
         //      state: 검색 버튼 상태     //
         const [status, setStatus] = useState<boolean>(false);
         //      state: 검색어 상태       //
         const [searchWord, setSearchWord] = useState('');
 
-        //      event handler: 검색 아이콘 클릭 이벤트 처리 함수      //
+        //      event handler: 검색어 변경 이벤트 처리 함수      //
         const onSearchWordChangeHandler = (event: ChangeEvent<HTMLInputElement>)=> {
             const value = event.target.value;
             setSearchWord(value);
         }
+        //      event handler: 검색어 키 처리 이벤트 함수      //
+        const onSearchWordKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+            if (event.key !== 'Enter') return;
+            if (!searchButtonRef.current) return;
+            searchButtonRef.current.click();
+        };
         //      event handler: 검색 아이콘 클릭 이벤트 처리 함수      //
         const onSearchButtonClickHandler = () => {
             if (!status) {
                 setStatus(!status);
                 return;
             }
+            navigate(SEARCH_PATH(searchWord));
         }
 
-        if (!status) {
-            //      render : 검색 버튼 컴포넌트 렌더링 (클릭 false 상태)    //
-            return (
-                <div className='icon-button' onClick={onSearchButtonClickHandler}>
+        if (!status)
+        //      render : 검색 버튼 컴포넌트 렌더링 (클릭 false 상태)    //
+        return (
+            <div className='icon-button' onClick={onSearchButtonClickHandler}>
+                <div className="icon search-light-icon"></div>
+            </div>
+        );
+
+        //      render : 검색 버튼 컴포넌트 렌더링 (클릭 true 상태)    //
+        return (
+            <div className='header-search-input-box'>
+                <input className='header-search-input' type='text' placeholder='검색어를 입력해주세요' value={searchWord} onChange={onSearchWordChangeHandler} onKeyDown={onSearchWordKeyDownHandler}/>
+                <div ref={searchButtonRef} className="icon-button" onClick={onSearchButtonClickHandler}>
                     <div className="icon search-light-icon"></div>
                 </div>
-            );
-        } else {
-            //      render : 검색 버튼 컴포넌트 렌더링 (클릭 true 상태)    //
-            return (
-                <div className='header-search-input-box'>
-                    <input className='header-search-input' type='text' placeholder='검색어를 입력해주세요' value={searchWord} onChange={onSearchButtonClickHandler}/>
-                    <div className="icon-button" >
-                        <div className="icon search-light-icon"></div>
-                    </div>
-                </div>
-            );
-        }
+            </div>
+        );
+
     }
 
     //      render: Header layout render     //
