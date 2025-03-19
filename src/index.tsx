d74bd414f4137e2bacd8691f816283da273b6414
tag101 <tag101@hanmail.net>
2025-03-20 01:52:44 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 20. Header 레이아웃 작성 B

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CookiesProvider } from "react-cookie";
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
@@ -9,8 +10,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+      <CookiesProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+      </CookiesProvider>
   </React.StrictMode>
 );
