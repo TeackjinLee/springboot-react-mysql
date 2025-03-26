961110a849466abd392180d4f9b892d2dd6bcb55
tag101 <tag101@hanmail.net>
2025-03-27 08:19:12 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 28. Auth 페이지 로그인 카드 B

--- src/apis/request/auth/index.ts
+++ src/apis/request/auth/index.ts
@@ -1,5 +1,7 @@
 import SignUpRequestDto from './sign-up.request.dto';
+import SignInRequestDto from "./Sign-in.request.dto";
 
 export type {
-    SignUpRequestDto
+    SignUpRequestDto,
+    SignInRequestDto
 }
