3045faaf681141eaf0eeb01d1b500e645d69e901
tag101 <tag101@hanmail.net>
2025-03-30 05:00:31 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 34. Auth 페이지 회원가입 API 연동

--- src/apis/request/auth/sign-up.request.dto.ts
+++ src/apis/request/auth/sign-up.request.dto.ts
@@ -2,6 +2,7 @@ export default interface SignUpRequestDto {
     email: string;
     password: string;
     nickname: string;
+    telNumber: string;
     address: string;
     addressDetail: string | null;
     agreedPersonal: boolean;
