30a64c2dabd405545c84ba3cf1d83b507fd4dfe5
tag101 <tag101@hanmail.net>
2025-04-03 00:17:31 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 40. 게시물 작성 페이지 스타일 작성

--- src/views/Board/Write/index.tsx
+++ src/views/Board/Write/index.tsx
@@ -39,12 +39,18 @@ export default function BoardWrite() {
                             <div className="icon image-box-light-icon"></div>
                         </div>
                         <input ref={imageInputRef} type="file" accept="image/*" style={{display: 'none'}}/>
+                    </div>
+                    <div className="board-write-images-box">
                         <div className="board-write-image-box">
-                            <div className="board-write-image-box">
-                                <img src="" alt="" className='board-write-image'/>
-                                <div className="icon-button image-close">
-                                    <div className="icon close-icon"></div>
-                                </div>
+                            <img className='board-write-image' src="https://media.istockphoto.com/id/520700958/ko/%EC%82%AC%EC%A7%84/%EC%95%84%EB%A6%84%EB%8B%A4%EC%9A%B4-%EA%BD%83-%EB%B0%B0%EA%B2%BD%EA%B8%B0%EC%88%A0.jpg?s=612x612&w=0&k=20&c=gJx5-O9U1qXKZqKwv4KunrBae7RDNRcdse1nOdSk_0w=" />
+                            <div className="icon-button image-close">
+                                <div className="icon close-icon"></div>
+                            </div>
+                        </div>
+                        <div className="board-write-image-box">
+                            <img className='board-write-image' src="https://media.istockphoto.com/id/520700958/ko/%EC%82%AC%EC%A7%84/%EC%95%84%EB%A6%84%EB%8B%A4%EC%9A%B4-%EA%BD%83-%EB%B0%B0%EA%B2%BD%EA%B8%B0%EC%88%A0.jpg?s=612x612&w=0&k=20&c=gJx5-O9U1qXKZqKwv4KunrBae7RDNRcdse1nOdSk_0w=" />
+                            <div className="icon-button image-close">
+                                <div className="icon close-icon"></div>
                             </div>
                         </div>
                     </div>
