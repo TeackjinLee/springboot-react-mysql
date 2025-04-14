8f78680572a1cdd3a75e5ed52dce1d477bf7e239
tag101 <tag101@hanmail.net>
2025-04-14 23:29:56 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 49. 게시물 상세보기 하단 컴포넌트 구조 작성

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.css'
+
+//      component: 페이지네이션 컴포넌트       //
+export default function Pagenation() {
+
+    //      render: 게시물 상세 하단 컴포넌트 렌더링       //
+    return (
+        <div>페이지화면</div>
+    )
+}
