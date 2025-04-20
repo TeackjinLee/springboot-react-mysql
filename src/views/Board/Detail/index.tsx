afc34eb1363c26215b0079cf84e8db3687a3ec92
tag101 <tag101@hanmail.net>
2025-04-20 12:37:23 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 50. 게시물 상세보기 상단 스타일 작성

--- src/views/Board/Detail/index.tsx
+++ src/views/Board/Detail/index.tsx
@@ -5,11 +5,21 @@ import {CommentListItem, FavoriteListItem} from "../../../types/interface";
 import {commentListMock, favoriteListMock} from "../../../mocks";
 import CommentItem from "../../../components/CommentItem";
 import Pagenation from "../../../components/Pagination";
+import defaultProfileImage from 'assets/image/default-profile-image.png'
 
 //      component: 게시물 상세 화면 컴포넌트       //
 export default function BoardDetail() {
     //      component: 게시물 상세 상단 화면 컴포넌트       //
     const BoardDetailTop = () => {
+
+        //      state: more 버튼 상태       //
+        const [showMore, setShowMore] = useState<boolean>(false);
+
+        //      event handler: more 버튼 클릭 이벤트 처리        //
+        const onMoreButtonClickHandler = () => {
+            setShowMore(!showMore);
+        }
+
         //      render: 게시물 상세 상단 화면 렌더링      //
         return (
             <div id="board-detail-top">
@@ -17,25 +27,27 @@ export default function BoardDetail() {
                     <div className="board-detail-title">{'오늘 점심 뭐먹지? 추천 부탁드립니다.'}</div>
                     <div className="board-detail-top-sub-box">
                         <div className="board-detail-write-info-box">
-                            <div className="board-detail-writer-profile-image"></div>
+                            <div className="board-detail-writer-profile-image" style={{backgroundImage: `url(${defaultProfileImage})`}}></div>
                             <div className="board-detail-writer-nickname">{'안녕코린이'}</div>
                             <div className="board-detail-info-divider">{'\|'}</div>
                             <div className="boarcd-detail-writer-date">{'2025. 05. 12'}</div>
                         </div>
                         <div className="icon-button">
-                            <div className="icon more-icon"></div>
+                            <div className="icon more-icon" onClick={onMoreButtonClickHandler}></div>
                         </div>
+                        {showMore &&
                         <div className="board-detail-more-box">
                             <div className="board-detail-update-button">{'수정'}</div>
                             <div className="divider"></div>
                             <div className="board-detail-delete-button">{'삭제'}</div>
                         </div>
+                        }
                     </div>
                 </div>
                 <div className="divider"></div>
                 <div className="board-detail-top-main">
-                    <div className="baord-detail-top-main">{'오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나점심 안먹음...'}</div>
-                    <div className="board-detail-main-image"></div>
+                    <div className="board-detail-main-text">{'오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나점심 안먹음...'}</div>
+                    <img className="board-detail-main-image" src='https://images.mypetlife.co.kr/content/uploads/2021/10/19151330/corgi-g1a1774f95_1280-1024x682.jpg'/>
                 </div>
             </div>
         );
