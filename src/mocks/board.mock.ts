9ae0f608f2e7bb14d53ef626455839b38f6c9a17
tag101 <tag101@hanmail.net>
2025-04-21 23:56:43 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 52. 게시물 상세 페이지 이벤트 처리

---
+++ src/mocks/board.mock.ts
@@ -0,0 +1,15 @@
+import {Board} from "../types/interface";
+
+const boardMock: Board = {
+    boardSeq: 1,
+    title: '제목.................',
+    content: '내용들어감.............................',
+    boardImageList: ['https://blog.malcang.com/wp-content/uploads/2024/03/1-1.png',
+                    'https://cdnweb01.wikitree.co.kr/webdata/editor/202408/16/img_20240816175532_b82de03f.webp'],
+    writeDatetime:'2025.05.12',
+    writerEmail: 'email.com',
+    writeNickname: '코린이',
+    writeProfileImage: 'https://image.utoimage.com/preview/cp872722/2022/12/202212008462_500.jpg',
+}
+
+export default boardMock;
