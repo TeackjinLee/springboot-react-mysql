745e335a9fdd879758aaacec9879eaabcde376d5
tag101 <tag101@hanmail.net>
2025-04-24 23:36:17 +0900

    [Spring boot + Reactjs(ts) + MySQL] - 55. 게시물 상세 페이지 좋아요, 댓글 작성, 게시물 삭제 API 연동

--- src/views/Board/Detail/index.tsx
+++ src/views/Board/Detail/index.tsx
@@ -1,18 +1,21 @@
 import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
 import './style.css';
-import FavoriteItem from "../../../components/FavoriteItem";
-import {Board, CommentListItem, FavoriteListItem} from "../../../types/interface";
-import {boardMock, commentListMock, favoriteListMock} from "../../../mocks";
-import CommentItem from "../../../components/CommentItem";
-import Pagenation from "../../../components/Pagination";
+import FavoriteItem from "components/FavoriteItem";
+import {Board, CommentListItem, FavoriteListItem} from "types/interface";
+import { commentListMock, favoriteListMock } from "mocks";
+import CommentItem from "components/CommentItem";
+import Pagenation from "components/Pagination";
 import defaultProfileImage from 'assets/image/default-profile-image.png'
-import {useLoginUserStore} from "../../../stores";
+import {useLoginUserStore} from "stores";
 import {useNavigate, useParams} from "react-router-dom";
-import {BOARD_PATH, BOARD_UPDATE_PATH, MAIN_PATH, USER_PATH} from "../../../constant";
-import {getBoardRequest, increaseViewCountRequest} from "../../../apis";
-import GetBoardResponseDto from "../../../apis/response/board/get-board.response.dto";
-import {ResponseDto} from "../../../apis/response";
-import {IncreaseViewCountResponseDto} from "../../../apis/response/board";
+import {BOARD_PATH, BOARD_UPDATE_PATH, MAIN_PATH, USER_PATH} from 'constant';
+import {getBoardRequest, getCommentListRequest, getFavoriteListRequest, increaseViewCountRequest} from "apis";
+import GetBoardResponseDto from "apis/response/board/get-board.response.dto";
+import {ResponseDto} from "apis/response";
+import {IncreaseViewCountResponseDto} from "apis/response/board";
+
+import dayjs from 'dayjs';
+import {GetCommentListResponseDto, GetFavoriteListResponseDto} from "../../../apis/response/board";
 
 //      component: 게시물 상세 화면 컴포넌트       //
 export default function BoardDetail() {
@@ -43,7 +46,14 @@ export default function BoardDetail() {
         //      state: more 버튼 상태       //
         const [showMore, setShowMore] = useState<boolean>(false);
 
-        //      function get board response 처리 함수       //
+        //      function: 작성일 포멧 변경 처리 함수       //
+        const getWriteDatetimeFormat = () => {
+            if (!board) return '';
+            const date = dayjs(board.writeDatetime);
+            return date.format('YYYY. MM. DD.');
+        }
+
+        //      function: get board response 처리 함수       //
         const getBoardResponse = (responseBody: GetBoardResponseDto | ResponseDto | null) => {
             if (!responseBody) return;
             const {code} = responseBody;
@@ -107,7 +117,7 @@ export default function BoardDetail() {
                             <div className="board-detail-writer-profile-image" style={{backgroundImage: `url(${board.writeProfileImage ? board.writeProfileImage : defaultProfileImage})`}}></div>
                             <div className="board-detail-writer-nickname" onClick={onNicknameClickHandler}>{board.writeNickname}</div>
                             <div className="board-detail-info-divider">{'\|'}</div>
-                            <div className="boarcd-detail-writer-date">{board.writeDatetime}</div>
+                            <div className="boarcd-detail-writer-date">{getWriteDatetimeFormat()}</div>
                         </div>
                         {isWriter &&
                         <div className="icon-button">
@@ -151,6 +161,36 @@ export default function BoardDetail() {
         const onFavoriteClickHandler = () => {
             setFavorite(!isFavorite);
         }
+        //      function: get favorite list response 처리 함수        //
+        const getFavoriteListResponse = (responseBody: GetFavoriteListResponseDto | ResponseDto | null) => {
+            if (!responseBody) return;
+            const { code } = responseBody;
+            if (code === 'NB') alert('존재하지 않는 게시물입니다.');
+            if (code === 'DBE') alert('데이터베이스 오류입니다.');
+            if (code !== 'SU') return;
+
+            const { favoriteList } = responseBody as GetFavoriteListResponseDto;
+            setFavoriteList(favoriteList);
+            if (!loginUser) {
+                setFavorite(false);
+                return;
+            }
+            const isFavorite = favoriteList.findIndex(favorite => favorite.email === loginUser.email) !== -1;
+            setFavorite(isFavorite);
+        }
+        //      function: get comment list response 처리 함수       //
+        const getCommentListResponse = (responseBody: GetCommentListResponseDto | ResponseDto | null) => {
+            if (!responseBody) return;
+            const { code } = responseBody;
+            if (code === 'NB') alert('존재하지 않는 게시물입니다.');
+            if (code === 'DBE') alert('데이터베이스 오류입니다.');
+            if (code !== 'SU') return;
+
+            const { commentList } = responseBody as GetCommentListResponseDto;
+            setCommentList(commentList);
+        }
+
+
         //      event handler: 좋아요 상자 보기 클릭 이벤트 처리        //
         const onShowFavoriteClickHandler = () => {
             setShowFavorite(!showFavorite);
@@ -175,8 +215,9 @@ export default function BoardDetail() {
 
         //      effect: 게시물 번호 path variable이 바뀔때마다 좋아요 및 댓글 리스트 불러오기       //
         useEffect(() => {
-            setFavoriteList(favoriteListMock);
-            setCommentList(commentListMock);
+            if (!boardSeq) return;
+            getFavoriteListRequest(boardSeq).then(getFavoriteListResponse);
+            getCommentListRequest(boardSeq).then(getCommentListResponse);
         }, [boardSeq]);
 
         //      render: 게시물 상세 하단 화면 렌더링      //
@@ -214,7 +255,7 @@ export default function BoardDetail() {
                 {showFavorite &&
                 <div className="board-detail-bottom-favorite-box">
                     <div className="board-detail-bottom-favorite-container">
-                        <div className="board-detail-bottom-favorite-title">{'좋아요 '}<span className='emphasis'>{12}</span></div>
+                        <div className="board-detail-bottom-favorite-title">{'좋아요 '}<span className='emphasis'>{favoriteList.length}</span></div>
                         <div className="board-detail-bottom-favorite-contents">
                             {favoriteList.map(item => <FavoriteItem favoriteListItem={item}/>)}
                         </div>
@@ -224,7 +265,7 @@ export default function BoardDetail() {
                 {showComment &&
                 <div className="board-detail-bottom-comment-box">
                     <div className="board-detail-bottom-comment-container">
-                        <div className="board-detail-bottom-comment-title">{'댓글'}<span className='emphasis'>{12}</span></div>
+                        <div className="board-detail-bottom-comment-title">{'댓글'}<span className='emphasis'>{commentList.length}</span></div>
                         <div className="board-detail-bottom-comment-list-container">
                             {commentList.map(item => <CommentItem commentListItem={item} />)}
                         </div>
@@ -233,6 +274,7 @@ export default function BoardDetail() {
                     <div className="board-detail-bottom-comment-pagination-box">
                         <Pagenation/>
                     </div>
+                    {loginUser !== null &&
                     <div className="board-detail-bottom-comment-input-box">
                         <div className="board-detail-bottom-comment-input-container">
                             <textarea ref={commentRef} className='board-detail-bottom-comment-textarea' placeholder='댓글을 작성해주세요.' value={comment} onChange={onCommentChangeHandler}></textarea>
@@ -241,6 +283,7 @@ export default function BoardDetail() {
                             </div>
                         </div>
                     </div>
+                    }
                 </div>}
             </div>
         );
@@ -254,9 +297,7 @@ export default function BoardDetail() {
             effectFlag = false;
             return;
         }
-        console.log("1111");
         increaseViewCountRequest(boardSeq).then(increaseViewCountResponse);
-        console.log("11112");
     }, [boardSeq]);
     //      render: 게시물 상세 화면 렌더링      //
     return (
